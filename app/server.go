1fedfaab762531144d2f9e8e2dce5de5161b841b
Amjad Jibon <amjad.jibon@gmail.com>
2024-03-10 18:04:42 +0800

    stage1: implement basic tcp server

--- app/server.go
+++ app/server.go
@@ -1,26 +1,22 @@
 package main
 
 import (
-	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"log"
+	"net"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	var addr = "0.0.0.0:6379"
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	log.Println("start listening on", addr)
+
+	listen, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	_, err = listen.Accept()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
